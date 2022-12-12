eb131655572cf2d5ba5acb9daaf6a6b3e04306f3
Jogi <stigmahotspot@gmail.com>
2022-12-12 14:09:52 +0700

    pertama8

--- js.js
+++ js.js
@@ -2,8 +2,8 @@ var b = document.querySelector("button.button_no");
 b.addEventListener("click",change);
 function change()
 {
-    var i = Math.floor(Math.random()*100)+1;
-    var j = Math.floor(Math.random()*100)+1;
+    var i = Math.floor(Math.random()*200)+1;
+    var j = Math.floor(Math.random()*200)+1;
     b.style.right = i+"px";
     b.style.top = j+"px";
 }
